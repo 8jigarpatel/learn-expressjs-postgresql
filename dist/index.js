ef6af88633f01896e2727ab5dd4e3aa867ad5177
Jigar Patel <8.jigar.patel@gmail.com>
2023-05-24 18:22:34 -0600

    fix: add prettier and eslint, update scripts

--- dist/index.js
+++ dist/index.js
@@ -3,12 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const port = 8000;
-const app = (0, express_1.default)();
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+var express_1 = __importDefault(require("express"));
+// Boot express
+var app = (0, express_1.default)();
+var port = 5000;
+// Application routing
+app.use('/', function (req, res) {
+    res.status(200).send({ data: 'Hello World!' });
 });
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-});
+// Start server
+// eslint-disable-next-line no-console
+app.listen(port, function () { return console.log("Server is listening on port ".concat(port, "!")); });
+//# sourceMappingURL=index.js.map
